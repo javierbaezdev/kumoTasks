c3f93845875298df88edd0a150cfdce76e43a785
javierbaezdev <javierbaez.dev@gmail.com>
2024-02-09 11:49:22 -0300

    up

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { VitePWA, VitePWAOptions } from 'vite-plugin-pwa'
 import path from 'path'
+
+const manifestForPlugin: Partial<VitePWAOptions> = {
+  registerType: 'autoUpdate',
+  manifest: {
+    name: 'Kumo tasks',
+    short_name: 'KT',
+    description: 'App para organizar tareas',
+    icons: [
+      {
+        src: '/icon.png',
+        sizes: '200x200',
+        type: 'image/png',
+        purpose: 'any maskable',
+      },
+    ],
+    theme_color: '#171717',
+    background_color: '#e8ebf2',
+    display: 'standalone',
+    scope: '/',
+    start_url: '/',
+    orientation: 'portrait',
+  },
+}
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,25 +33,5 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'prompt',
-      manifest: {
-        name: 'Kumo tasks',
-        short_name: 'KT',
-        description: 'App para organizar tareas',
-        icons: [
-          {
-            src: '/icon.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
-        ],
-        scope: '/',
-        start_url: '/',
-      },
-    }),
-  ],
+  plugins: [react(), VitePWA(manifestForPlugin)],
 })
