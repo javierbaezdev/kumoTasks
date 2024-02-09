00e2c61d45cc023ecfd46f067cfdb9cbf0d49aac
javierbaezdev <javierbaez.dev@gmail.com>
2024-02-09 11:26:45 -0300

    added pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,34 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react"
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
-  plugins: [react()],
-});
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'prompt',
+      manifest: {
+        name: 'Kumo tasks',
+        short_name: 'KT',
+        description: 'App para organizar tareas',
+        icons: [
+          {
+            src: '/icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+        scope: '/',
+        start_url: '/',
+      },
+    }),
+  ],
+})
